fb6526e941b229d981867d607463bca1ea38317d
Muhammad Rizqi <mrizqi070502@gmail.com>
2023-09-20 22:20:43 +0700

    feat: order schema

---
+++ models/order.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const orderSchema = new mongoose.Schema({
+  Id: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  items: [
+    {
+      product: {
+        type: String,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  karyawan: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Karyawan',
+  },
+})
+
+module.exports = mongoose.model('Order', orderSchema)
