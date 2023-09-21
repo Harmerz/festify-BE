c48064c5fe69ee396f659e6498c57e66efb81b74
Nafisa <nafisaramadhania1122@gmail.com>
2023-09-21 20:37:31 +0700

    add recipe crud raw

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ mongoose
 //middleware
 app.use(express.json())
 
-const karyawan = require('./routes/karyawan')
-app.use('/karyawan', karyawan)
+const recipe = require('./routes/recipe')
+app.use('/recipe', recipe)
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000')
