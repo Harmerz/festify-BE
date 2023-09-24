ba4a87c66be9184aaf36903adafc448605be94e5
nafisa <nafisaramadhania1122@gmail.com>
2023-09-24 21:29:57 +0700

    update router

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { getRecipe, addRecipe } = require('../controllers/recipe')
+const { getRecipe, getOneRecipe, addRecipe, deleteRecipe, updateRecipe } = require('../controllers/recipe')
 
 router.get('/', getRecipe)
+router.get('/:id', getOneRecipe)
 router.post('/', addRecipe)
+router.delete('/:id', deleteRecipe)
+router.patch('/:id', updateRecipe)
 
 module.exports = router
