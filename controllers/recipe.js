622d0840d46e98997e563aaeb1ee26571c867efa
nafisa <nafisaramadhania1122@gmail.com>
2023-09-24 21:29:07 +0700

    update controller

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -9,32 +9,32 @@ exports.getRecipe = (req, res) => {
     .catch((err) => console.log(err))
 }
 
+exports.getOneRecipe = (req, res) => {
+  const recipeId = req.params.id; // Assuming the ID is passed in the request parameters
+
+    Recipe.findById(recipeId)
+      .then((recipe) => {
+        if (!recipe) return res.status(404).json({ message: 'Recipe tidak ditemukan' });
+        return res.json(recipe);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json({ message: 'Internal Server Error' });
+      });
+}
+
 exports.addRecipe = (req, res) => {
   const recipe = new Recipe({
     Id: req.body.id,
     name: req.body.name,
     ingredient: req.body.ingredient,
-    instruction: req.body.instruction,
+    description: req.body.description,
   })
   recipe.save((err, recipe) => {
     if (err) return res.status(500).json({ error: err })
     return res.json(recipe)
   })
 }
-
-exports.updateRecipe = (req, res) => {
-  const { Id, name, ingredient, instruction } = req.body;
-
-  Recipe.findByIdAndUpdate(
-    req.params.id,
-    { Id, name, ingredient, instruction },
-    { new: true },
-    (err, recipe) => {
-      if (err) return res.status(500).json({ error: err });
-      return res.json(recipe);
-    }
-  );
-};
 
 exports.deleteRecipe = (req, res) => {
   Recipe.findByIdAndDelete(req.params.id, (err) => {
@@ -43,3 +43,16 @@ exports.deleteRecipe = (req, res) => {
   });
 };
 
+exports.updateRecipe = (req, res) => {
+  const { Id, name, ingredient, description } = req.body;
+
+  Recipe.findByIdAndUpdate(
+    req.params.id,
+    { Id, name, ingredient, description },
+    { new: true },
+    (err, recipe) => {
+      if (err) return res.status(500).json({ error: err });
+      return res.json(recipe);
+    }
+  );
+};
