f1a4f95c10f81daa799763ff661bb8fef2fcfb7b
nafisa <nafisaramadhania1122@gmail.com>
2023-09-25 16:31:03 +0700

    add swagger

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
 require('dotenv').config()
 
 mongoose
@@ -22,6 +24,23 @@ app.use(express.json())
 
 const recipe = require('./routes/recipe')
 app.use('/recipe', recipe)
+
+//swagger config
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Inventory API',
+      version: '1.0.0',
+    },
+  },
+  apis:  ['./routes/inventory.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+//serve swagger ui
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000')
--- routes/recipe.js
+++ routes/recipe.js
@@ -2,10 +2,102 @@ const express = require('express')
 const router = express.Router()
 const { getRecipe, getOneRecipe, addRecipe, deleteRecipe, updateRecipe } = require('../controllers/recipe')
 
+/**
+ * @swagger
+ * tags:
+ *   name: Recipe
+ *   description: Recipe operations
+ */
+
+
+//read-swagger
+/**
+ * @swagger
+ * /recipe:
+ *   get:
+ *     summary: Get a list of recipe items
+ *     tags: [Recipe]
+ *     responses:
+ *       '200':
+ *         description: Successful response
+ */
 router.get('/', getRecipe)
+
+//read-swagger
+/**
+ * @swagger
+ * /recipe/{id}:
+ *   get:
+ *     summary: Get a recipe by ID
+ *     tags: [Recipe]
+ *     responses:
+ *       '200':
+ *         description: Successful response
+ *       '400':
+ *         description: Invalid status value
+ */
 router.get('/:id', getOneRecipe)
+
+//create-swagger
+/**
+ * @swagger
+ * /recipe:
+ *   post:
+ *     summary: Create a new recipe item
+ *     tags: [Recipe]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Recipe'
+ *     responses:
+ *       '201':
+ *         description: Recipe item created
+ */
 router.post('/', addRecipe)
+
+//delete-swagger
+/**
+ * @swagger
+ * /recipe/{id}:
+ *   delete:
+ *     summary: Delete an recipe item
+ *     tags: [Recipe]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               _id:
+ *                 type: string
+ *             required:
+ *               - _id
+ *     responses:
+ *       '200':
+ *         description: Recipe item deleted
+ */
 router.delete('/:id', deleteRecipe)
-router.patch('/:id', updateRecipe)
+
+//update-swagger
+/**
+ * @swagger
+ * /recipe/{id}:
+ *   put:
+ *     summary: Update an existing recipe item
+ *     tags: [Recipe]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Recipe'
+ *     responses:
+ *       '200':
+ *         description: Recipe item updated
+ */
+router.put('/:id', updateRecipe)
 
 module.exports = router
