44de73e4372c2f6a37fc1819b2293cee373707b3
krisnaarianta <krisnaarianta@gmail.com>
2023-09-25 13:29:37 +0700

    feat: swagger ui

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
 require('dotenv').config()
 
 mongoose
@@ -25,6 +27,23 @@ app.use('/karyawan', karyawan)
 
 const inventory = require('./routes/inventory')
 app.use('/inventory', inventory)
+
+//swagger config
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Inventory API',
+      version: '1.0.0',
+    },
+  },
+  apis:  ['./routes/inventory.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+//serve swagger ui
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.listen(5000, () => {
   console.log('Server is running on port 5000')
--- routes/inventory.js
+++ routes/inventory.js
@@ -2,9 +2,86 @@ const express = require('express')
 const router = express.Router()
 const { getInventory, addInventory, deleteInventory, updateInventory } = require('../controllers/inventory')
 
-router.get('/', getInventory)
-router.post('/', addInventory)
-router.put('/', updateInventory)
-router.delete('/', deleteInventory)
+/**
+ * @swagger
+ * tags:
+ *   name: Inventory
+ *   description: Inventory operations
+ */
+
+//read-swagger
+/**
+ * @swagger
+ * /inventory:
+ *   get:
+ *     summary: Get a list of inventory items
+ *     tags: [Inventory]
+ *     responses:
+ *       '200':
+ *         description: Successful response
+ */
+router.get('/', getInventory);
+
+//create-swagger
+/**
+ * @swagger
+ * /inventory:
+ *   post:
+ *     summary: Create a new inventory item
+ *     tags: [Inventory]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Inventory'
+ *     responses:
+ *       '201':
+ *         description: Inventory item created
+ */
+router.post('/', addInventory);
+
+//update-swagger
+/**
+ * @swagger
+ * /inventory:
+ *   put:
+ *     summary: Update an existing inventory item
+ *     tags: [Inventory]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Inventory'
+ *     responses:
+ *       '200':
+ *         description: Inventory item updated
+ */
+router.put('/', updateInventory);
+
+//delete-swagger
+/**
+ * @swagger
+ * /inventory:
+ *   delete:
+ *     summary: Delete an inventory item
+ *     tags: [Inventory]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               _id:
+ *                 type: string
+ *             required:
+ *               - _id
+ *     responses:
+ *       '200':
+ *         description: Inventory item deleted
+ */
+router.delete('/', deleteInventory);
 
 module.exports = router
